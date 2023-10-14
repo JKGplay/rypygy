abed267ca322b82d4f3f77a6de6bc2bca17f48d9
Jan Ganczarczyk <jkgplay2005@gmail.com>
2023-10-14 23:19:49 +0200

    Dodałem klasy Item, Sword, Shield, Potion

---
+++ app/src/main/java/com/example/rypygy/models/items/Shield.java
@@ -0,0 +1,29 @@
+package com.example.rypygy.models.items;
+
+import androidx.annotation.NonNull;
+
+import com.example.rypygy.models.Item;
+
+public class Shield extends Item {
+    private enum Types {WOODEN, STONE, IRON}
+    public Shield(@NonNull Types t) {
+        super(Category.SHIELD);
+        switch (t) {
+            case WOODEN:
+                this.setName("Wooden Shield");
+                this.setPrice(15);
+                setDefense(2);
+                break;
+            case STONE:
+                this.setName("Stone Shield");
+                this.setPrice(30);
+                setDefense(4);
+                break;
+            case IRON:
+                this.setName("Iron Shield");
+                this.setPrice(15);
+                setDefense(6);
+                break;
+        }
+    }
+}
