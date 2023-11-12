411e1079c464a38ff4f41eb1cc49a48e4b7e87e0
Jan Ganczarczyk <jkgplay2005@gmail.com>
2023-11-12 22:59:06 +0100

    Dodanie klasy Encounter, odpowiedzialniej za losowanie encounteru po otrzymaniu danej lokacji.

--- app/src/main/java/com/example/rypygy/SecondActivity.java
+++ app/src/main/java/com/example/rypygy/SecondActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rypygy.models.Character;
+import com.example.rypygy.models.Encounter;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -63,7 +65,8 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
                 break;
             case "btnShop":
 //                startActivity(new Intent(MainActivity2.this, ShopActivity.class));
-                Toast.makeText(this, "IN PROGRESS", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "IN PROGRESS", Toast.LENGTH_SHORT).show();
+                Log.d("test encounteru", String.valueOf(Encounter.outcome(Encounter.Location.FOREST)));
                 break;
             default:
                 Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/rypygy/models/Encounter.java
@@ -0,0 +1,67 @@
+package com.example.rypygy.models;
+
+import android.util.Log;
+
+import java.util.stream.IntStream;
+
+public class Encounter {
+
+    /*
+        Cała klasa skupia się na metodzie outcome.
+        Klasa zostaje wywołana w momencie kliknięcia przycisku lokacji w SecondActivity.
+        Outcome przyjmuje parametr location (FOREST, GARAGES, itp.)
+        Zwraca wynik spotkania (encounteru) np. walka, spotkanie npc, znalezienie itemu lub złota.
+        Możliwe jest określenie wartości procentowych na dany encounter; możliwa jest również wartość 0%
+     */
+
+    public enum Location {
+        FOREST,
+        GARAGES,
+        TOILETS,
+        COMPUTER_ROOM,
+        INTERNAT,
+        KACZYCE
+    }
+    public enum Type {
+        COMBAT,
+        NPC,
+        ITEM,
+        GOLD
+    }
+
+    public static Type outcome(Location location) {
+        Type toReturn = null;
+        switch (location) {
+            case FOREST:
+                toReturn = Encounter.percentage(new int[] {50, 15, 20, 15});
+                break;
+            case GARAGES:
+            case TOILETS:
+            case COMPUTER_ROOM:
+            case INTERNAT:
+            case KACZYCE:
+                break;
+        }
+        return toReturn;
+    }
+
+    public static Type percentage(int[] percentages) {
+        int rnd = Rnd.rnd(1, 100);
+        Log.d("rnd = ", "rnd = " + rnd);
+        int i = 0;
+        int value = percentages[i];
+        Type ret = null;
+        if (IntStream.of(percentages).sum() != 100) {
+            return null;
+        }
+        for (Type t : Type.values()) {
+            if (value >= rnd) {
+                ret = t;
+                break;
+            }
+            i++;
+            value += percentages[i];
+        }
+        return ret;
+    }
+}
